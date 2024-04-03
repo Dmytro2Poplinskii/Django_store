675418c1bfb00c05de00320475b4151ea252d57e
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-04-03 12:44:42 +0300

    feat: created Product model

--- store_app/models.py
+++ store_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+CATEGORY_CHOICES = (
+    ("CR", "Curd"),
+    ("ML", "Milk"),
+    ("LS", "Lassi"),
+    ("MS", "Milkshake"),
+    ("PN", "Panner"),
+    ("GH", "Ghee"),
+    ("CZ", "Cheese"),
+    ("IC", "Ice-Greans"),
+)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    composition = models.TextField(default="")
+    prod_app = models.TextField(default="")
+    category = models.CharField(max_length=2, choices=CATEGORY_CHOICES)
+    product_image = models.ImageField(upload_to="product")
+
+    def __str__(self) -> str:
+        return f"{self.title}"
