257704f8df155e8256bf9048daf727dd7e2dc173
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-04-03 17:33:08 +0300

    fix: fix problem with downloading images

--- store_app/urls.py
+++ store_app/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 
 from . import views
 
-
 urlpatterns = [
-    path('home/', views.home, name="home"),
-    path('category/<slug:name>', views.CategoryView.as_view(), name="category"),
+    path("home/", views.home, name="home"),
+    path("category/<slug:name>", views.CategoryView.as_view(), name="category"),
 ]
 
 app_name = "store_app"
