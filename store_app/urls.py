3ec210518c613b17ebe8c68c4ed4d4bebf33eb48
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-04-03 14:46:52 +0300

    feat: added category template and CategoryView

--- store_app/urls.py
+++ store_app/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name="home"),
+    path('category/<slug:name>', views.CategoryView.as_view(), name="category"),
 ]
 
 app_name = "store_app"
--- store_app/views.py
+++ store_app/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from django.views import View
 
 
 def home(request: HttpRequest) -> HttpResponse:
     return render(request, "app/home.html")
+
+
+class CategoryView(View):
+    def get(self, request: HttpRequest, name: str) -> HttpResponse:
+        return render(request, "app/category.html", locals())
