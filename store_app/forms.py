cc299d7f02d05816e2a8037421c1b38ebf1d5abb
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-04-09 12:45:31 +0300

    fix: fix problem with correct saving new user

--- store_app/forms.py
+++ store_app/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.contrib.auth.models import User
 
 
 class CustomerRegistrationForm(UserCreationForm):
@@ -15,3 +16,7 @@ class CustomerRegistrationForm(UserCreationForm):
         widget=forms.PasswordInput(attrs={"class": "form-control"}),
         label="Confirm Password",
     )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
