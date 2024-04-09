f9f8cdcf3f9098a455d62d21ea42d036545d13df
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-04-09 21:28:38 +0300

    fix: fix names for password reset

--- store_service/urls.py
+++ store_service/urls.py
@@ -16,18 +16,18 @@ urlpatterns = [
     path("password-reset/", auth_views.PasswordResetView.as_view(
         template_name="app/password_reset.html",
         form_class=forms.CustomerPasswordResetForm,
-    ), name="password-reset"),
+    ), name="password_reset"),
     path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(
         template_name="app/password_reset_done.html"
-    ), name="password-reset-done"),
+    ), name="password_reset_done"),
     path("password-reset/confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(
         template_name="app/password_reset_confirm.html",
         form_class=forms.CustomerPasswordSetForm,
         success_url="/store/password-reset-done/",
-    ), name="password-reset-confirm"),
+    ), name="password_reset_confirm"),
     path("password-reset/complete/", auth_views.PasswordResetCompleteView.as_view(
         template_name="app/password_reset_complete.html"
-    ), name="password-reset-complete"),
+    ), name="password_reset_complete"),
     path("login/", auth_views.LoginView.as_view(
         template_name="app/login.html",
         authentication_form=forms.CustomerLoginForm
