f712c32d40419b31d083e2d8f11a1d2c04098a9b
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-04-13 15:11:41 +0300

    feat: added function for checking address in scripts.js

--- store_app/static/app/js/script.js
+++ store_app/static/app/js/script.js
@@ -1,61 +1,57 @@
-document.addEventListener("DOMContentLoaded", function() {
-    $(".plus-cart").click(function() {
-        let id = $(this).data("pid")
-        let el = this.parentNode.children[2]
+$(".plus-cart").click(function() {
+    let id = $(this).data("pid")
+    let el = this.parentNode.children[2]
 
-        $.ajax({
-            type: "GET",
-            url: "/store/plus-cart",
-            data: {
-                product_id: id
-            },
-            success: function (data) {
-                el.innerText = data.quantity
-                document.getElementById("amount").innerText = data.amount
-                document.getElementById("total_amount").innerText = "$ " + data.total_amount
-            }
-        })
-    })
-
-    $(".minus-cart").click(function() {
-        let id = $(this).data("pid")
-        let el = this.parentNode.children[2]
-
-        $.ajax({
-            type: "GET",
-            url: "/store/minus-cart",
-            data: {
-                product_id: id
-            },
-            success: function (data){
-                el.innerText = data.quantity
-                document.getElementById("amount").innerText = data.amount
-                document.getElementById("total_amount").innerText = "$ " + data.total_amount
-            }
-        })
-    })
-
-    $(".remove-cart").click(function() {
-        let id = $(this).data("pid")
-        let el = $(this).closest(".row")
-
-        $.ajax({
-            type: "GET",
-            url: "/store/remove-cart",
-            data: {
-                product_id: id
-            },
-            success: function (data) {
-                document.getElementById("amount").innerText = data.amount
-                document.getElementById("total_amount").innerText = "$ " + data.total_amount
-                el.remove()
-            }
-        })
+    $.ajax({
+        type: "GET",
+        url: "/store/plus-cart",
+        data: {
+            product_id: id
+        },
+        success: function (data) {
+            el.innerText = data.quantity
+            document.getElementById("amount").innerText = data.amount
+            document.getElementById("total_amount").innerText = "$ " + data.total_amount
+        }
     })
 })
 
+$(".minus-cart").click(function() {
+    let id = $(this).data("pid")
+    let el = this.parentNode.children[2]
 
+    $.ajax({
+        type: "GET",
+        url: "/store/minus-cart",
+        data: {
+            product_id: id
+        },
+        success: function (data){
+            el.innerText = data.quantity
+            document.getElementById("amount").innerText = data.amount
+            document.getElementById("total_amount").innerText = "$ " + data.total_amount
+        }
+    })
+})
 
+$(".remove-cart").click(function() {
+    let id = $(this).data("pid")
+    let el = $(this).closest(".row")
 
+    $.ajax({
+        type: "GET",
+        url: "/store/remove-cart",
+        data: {
+            product_id: id
+        },
+        success: function (data) {
+            document.getElementById("amount").innerText = data.amount
+            document.getElementById("total_amount").innerText = "$ " + data.total_amount
+            el.remove()
+        }
+    })
+})
 
-
+$('input[type=radio][name=customer_id]').change(function() {
+    $('#selected_customer_id').val($(this).val());
+});
