723ef03e3df8d0ef5d997250dd0f25a1a9baa040
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-05-16 11:14:04 +0300

    feat: added RedirectLogin middleware

--- store_service/settings.py
+++ store_service/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "store_app.middleware.RedirectMiddleware",
 ]
 
 ROOT_URLCONF = "store_service.urls"
